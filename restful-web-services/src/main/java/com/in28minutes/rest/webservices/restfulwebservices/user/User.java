b2adbed42adf3fa6133008140d868c952526335e
QuyetCN05 <buihuuquyet203@gmail.com>
2023-11-20 22:34:49 +0700

    final update

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,22 +1,45 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2)
     private String name;
     @Past
     private LocalDate birthDate;
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     public Integer getId() {
@@ -49,6 +72,7 @@ public class User {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", birthDate=" + birthDate +
+                ", posts=" + posts +
                 '}';
     }
 }
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user_details(BIRTH_DATE,ID,NAME) values
+(current_date(),10001,'Ranga'),
+(current_date(),10002,'Quyet Bui'),
+(current_date(),10003,'Harry');
+
+
+insert into Post(ID,USER_ID,DESCRIPTION) values
+(20001,10001,'I want to learn AWS'),
+(20002,10001,'I want to learn DevOps'),
+(20003,10002,'I want to learn GCP'),
+(20004,10002,'I want to learn Multi CLoud');
